e5000ce890e399a31869935235e445c88308a566
Your Name <you@example.com>
2018-12-23 10:26:20 +0700

    天気APIからデータ取得して画面実装

--- src/app/current-weather/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../../icurrent-weather';
 import { WeatherService } from '../../weather/weather.service';
 @Component({
   selector: 'app-current-weather',
@@ -6,8 +7,20 @@ import { WeatherService } from '../../weather/weather.service';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.svg',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather;
+    console.log(this.current);
+
+   }
 
   ngOnInit() {
   }
